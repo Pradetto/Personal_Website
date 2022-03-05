4e57eab11ddd723781e6291933688d67cfb45d6f
Michael <pradetto5@gmail.com>
2022-03-05 14:41:39 -0700

    Fixed file structure again

--- projects/sorting_algo_visualizer/Visualizer.py
+++ projects/sorting_algo_visualizer/Visualizer.py
@@ -1,1 +1,8 @@
-#Test 123 Test 123
+# Remember this is to get the requirements
+
+# pip3 freeze > requirements.txt  
+# pip3 install -r requirements.txt 
+# cd into sorting_algo_visualizer
+# activate virtual environment through source venv/bin/activate
+
+
