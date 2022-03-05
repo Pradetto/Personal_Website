0639cd9c5f440d749365caf6c0465ff0f108f7b7
Michael <pradetto5@gmail.com>
2022-03-05 14:44:13 -0700

    Fixed File Structure

--- sorting_algo_visualizer/Visualizer.py
+++ sorting_algo_visualizer/Visualizer.py
@@ -1,8 +1,7 @@
 # Remember this is to get the requirements
 
 # pip3 freeze > requirements.txt  
-# pip3 install -r requirements.txt 
 # cd into sorting_algo_visualizer
 # activate virtual environment through source venv/bin/activate
-
+# pip3 install -r requirements.txt 
 
